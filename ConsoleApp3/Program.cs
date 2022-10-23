e1509a4ab9839fd34c0bfe964754b57a9a6a8ac4
Anna <anyaamdmitrieva@yandex.ru>
2022-10-23 19:34:52 +0600

    Create ex.3

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+number = Math.Abs(number);
+
+if ( number == 1 || number == 2 || number == 3 || number == 4 || number == 5 )
+{
+    Console.WriteLine("Сегодня не выходной день.");
+}
+
+if ( number == 6 || number == 7 )
+{
+    Console.WriteLine("Выходной!");
+}
+
+if ( number < 1 || number > 7 )
+{
+    Console.WriteLine("Введите число от 1 до 7!");
+}
