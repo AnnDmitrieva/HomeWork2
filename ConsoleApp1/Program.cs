8b9391cf50d870acc71c4c244322f2f4035303a1
Anna <anyaamdmitrieva@yandex.ru>
2022-10-23 15:54:46 +0600

    Create ex.1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
+number = Math.Abs(number);
+
+int a1 = number % 10;
+int number2 = ( number - a1 ) / 10;
+int a2 =  number2 % 10;
+int number3 = ( number2 - a2 ) / 10;
+int number4 = number3 * 10 + a1;
+
+if ( number3 < 1 )
+{
+     Console.WriteLine("Вы ввели двухзначное число");
+}
+ else
+{
+     Console.WriteLine(a2);
+}
+    
+   
