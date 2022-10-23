71b49a15c64986abd4c165ded984c5ba21e60777
Anna <anyaamdmitrieva@yandex.ru>
2022-10-23 17:45:29 +0600

    Create ex.2 and modif ex1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,6 @@ int a1 = number % 10;
 int number2 = ( number - a1 ) / 10;
 int a2 =  number2 % 10;
 int number3 = ( number2 - a2 ) / 10;
-int number4 = number3 * 10 + a1;
 
 if ( number3 < 1 )
 {
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+number = Math.Abs(number);
+
+if (number < 99)
+{
+    Console.WriteLine("Вы ввели не трехзначное число, третьего числа нет.");
+}
+
+else
+{
+    if (number < 1000)
+    {
+        int rem = number % 10;
+        Console.WriteLine(rem);
+    }
+    else
+    {
+        while (number >= 1000)
+        {
+            int rem1 = number % 10;
+            number = (number - rem1) / 10;
+        }
+    int rem = number % 10;
+    Console.WriteLine(rem);
+    }
+}
